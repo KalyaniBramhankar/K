c6d7255e48e6bdacd69472d471ad12d9fc1e9499
jayas <jayas@LAPTOP-76HAPQ6V>
2023-09-11 21:47:47 +0530

    commit1

---
+++ src/sample/demo2.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class demo2 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hi");
+		System.out.println("hello");
+		
+		System.out.println("GM");
+		
+	}
+
+}
